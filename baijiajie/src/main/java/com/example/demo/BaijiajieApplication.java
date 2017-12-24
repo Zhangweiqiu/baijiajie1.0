bd3324378e627ecb6a2486da557b7494135fad13
qiuzhangwei <1125487148@qq.com>
2017-12-25 00:09:11 +0800

    不知道改了什么

--- baijiajie/src/main/java/com/example/demo/BaijiajieApplication.java
+++ baijiajie/src/main/java/com/example/demo/BaijiajieApplication.java
@@ -2,11 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class BaijiajieApplication {
+public class BaijiajieApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+           return application.sources(BaijiajieApplication.class);
+       }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaijiajieApplication.class, args);
